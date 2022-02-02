fa039fdb838195fd9a0b9735842210ca72f5ff39
Hassan Askar <hassanemad15697@gmail.com>
2022-02-02 06:22:55 +0100

    @After and @AfterReturning difference

--- src/com/spring/aop/Data.java
+++ src/com/spring/aop/Data.java
@@ -1,6 +1,7 @@
 package com.spring.aop;
 
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
@@ -17,8 +18,13 @@ public class Data {
 		System.out.println("Start Connection");
 	}
 	@After("execution(public * com.spring.*.*.*(..))")
-	public void end()
+	public void end1()
 	{
-		System.out.println("end Connection");
+		System.out.println("end1 Connection");
+	}
+	@AfterReturning("execution(public * com.spring.*.*.*(..))")
+	public void end2()
+	{
+		System.out.println("end2 Connection");
 	}
 }
